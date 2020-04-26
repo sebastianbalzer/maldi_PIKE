0b119628cbc26d753bfffa608b32f8935d69ca04
Sebastian <sebalzer@student.ethz.ch>
2020-04-26 08:05:33 +0200

    changed antibiotics

--- ismb2020_maldi/submit_baseline_jobs.sh
+++ ismb2020_maldi/submit_baseline_jobs.sh
@@ -6,7 +6,7 @@ TIME=23:59
 
 # s. aureus
 
-for A in Amoxicillin-Clavulansaeure Ciprofloxacin Penicillin; do
+for A in Ceftriaxon Ciprofloxacin Amoxicillin-Clavulansaeure; do
   for s in "${SEED[@]}"; do
     OUTPUT="${A}_saureus_seed${s}"
     bsub -N -W $TIME -o "${OUTPUT}_%J.json" -R "rusage[mem=${MEMORY}]" "poetry run python baseline.py --species saureus --antibiotic $A --seed $s"
@@ -16,7 +16,7 @@ done
 
 # e. coli
 
-for A in Amoxicillin-Clavulansaeure Ciprofloxacin Ceftriaxon; do
+for A in Ceftriaxon Ciprofloxacin Amoxicillin-Clavulansaeure; do
   for s in "${SEED[@]}"; do
     OUTPUT="${A}_ecoli_seed${s}"
     bsub -N -W $TIME -o "${OUTPUT}_%J.json" -R "rusage[mem=${MEMORY}]" "poetry run python baseline.py --species ecoli --antibiotic $A --seed $s"
@@ -26,7 +26,7 @@ done
 
 # k. pneu
 
-for A in Ciprofloxacin Ceftriaxon Piperacillin-Tazobactam; do
+for A in Ceftriaxon Ciprofloxacin Amoxicillin-Clavulansaeure; do
   for s in "${SEED[@]}"; do
     OUTPUT="${A}_kpneu_seed${s}"
     bsub -N -W $TIME -o "${OUTPUT}_%J.json" -R "rusage[mem=${MEMORY}]" "poetry run python baseline.py --species kpneu --antibiotic $A --seed $s"
